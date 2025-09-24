cd774538075aeb1df1b33967fb75cb1ecbf0870e
Lena Ickhorn <leickh@proton.me>
2025-09-24 23:06:13 +0200

    documentation(ini): Comment half of document structure
    The other half will be done later.

--- Ini/Core/inc-c/configury/ini/document.h
+++ Ini/Core/inc-c/configury/ini/document.h
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: MPL-2.0
 
-#ifndef CONFIGURY_INI_H
-#define CONFIGURY_INI_H
+#ifndef CONFIGURY_INI_DOCUMENT_H
+#define CONFIGURY_INI_DOCUMENT_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
 typedef struct IniDocument IniDocument;
+typedef struct IniSection IniSection;
+typedef struct IniField IniField;
+typedef struct IniValue IniValue;
 
 typedef enum
 {
@@ -27,31 +31,29 @@ struct IniValue
             uint8_t number_base;
             uint32_t num_digits;
             char *text;
-        };
-
-        struct IniHexadecimalInteger
-        {
-            uint32_t num_digits;
-            char *text;
-        };
+        } integer;
 
         struct IniString
         {
             uint32_t len_text;
             char *text;
-        };
+        } string;
 
         struct IniArray
         {
-            /// @brief Whether this array is represented as a list; with
-            ///        each entry in a new line and preceded by a minus.
+            /// @brief
+            ///     Whether this array is represented as a list; with
+            ///     each entry in a new line and preceded by a minus.
+            ///
+            ///     If this is FALSE, the list is encapsulated by square
+            ///     brackets, but may also span multiple lines.
             bool is_list;
             uint32_t num_items;
             IniValue *values;
 
             uint32_t num_indent_spaces;
-        };
-    };
+        } array;
+    } specifics;
 };
 
 struct IniField
@@ -69,24 +71,60 @@ struct IniSection
 
     uint32_t fields_capacity;
     uint32_t num_fields;
+
+    /// @brief
+    ///     To be interpreted as an array of pointers into the array
+    ///     `fields` of the `IniDocument` that also contains this section.
     IniField **fields;
 };
 
 struct IniDocument
 {
-    /// @brief The only instance of the INI source that the library is
-    ///        supposed to have for this document.
+    /// @brief
+    ///     The only instance of the INI source that the library is
+    ///     supposed to have for this document.
     ///
-    /// @note This source should be referenced but never copied.
+    /// @note
+    ///     This source should only be referenced but never copied.
     char *source;
 
+    /// @brief
+    ///     Number of allocated sections in the `sections`-array.
+    ///     This is often higher than `num_sections` to avoid
+    ///     re-allocating unnecessarily often.
     uint32_t sections_capacity;
+
+    /// @brief
+    ///     Number of items of the array `sections` that are actually used.
     uint32_t num_sections;
+
+    /// @brief
+    ///     Parts of the document, or sections. They start with a
+    ///     header consisting of the section's name which is encapsulated
+    ///     in the front with an opening square bracket and in the back
+    ///     with a closing square bracket.
+    ///
+    /// @note
+    ///     These structures may cross-reference into `fields`.
+    ///
+    /// @todo
+    ///     Make sure that there are no dangling pointers
+    ///     from `sections` into `fields`.
     IniSection *sections;
 
+    /// @brief
+    ///     Number of allocated fields in the `fields`-array.
+    ///     This is often higher than `num_fields` to avoid
+    ///     re-allocating unnecessarily often.
     uint32_t fields_capacity;
+
+    /// @brief
+    ///     Number of items of the array `fields` that are actually used.
     uint32_t num_fields;
+
+    /// @brief
+    ///     Generic INI-field with a name and a value of a given type.
     IniField *fields;
 };
 
-#endif // CONFIGURY_INI_H
+#endif // CONFIGURY_INI_DOCUMENT_H
